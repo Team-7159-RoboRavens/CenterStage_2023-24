2768ba55016672881a6839dba8d06983c0fe6ae4
Karsten <karstenyin@gmail.com>
2023-12-03 23:17:04 -0800

    attempt machine vision stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/PositionMarkers.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import android.graphics.Canvas;
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class PositionMarkers implements VisionProcessor {
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+
+    }
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        return frame;
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ArmTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ArmTuner.java
@@ -52,11 +52,33 @@ public class ArmTuner extends OpMode {
             }
         }
 
+        if (gamepad2.right_trigger > 0.1) {
+            if (robot.linearSlidesMotor1.getCurrentPosition() < -5 || robot.linearSlidesMotor2.getCurrentPosition() < -5) {
+                telemetry.addData("LS Direction", "INHIBIT DOWN");
+                robot.linearSlidesMotor1.setPower(0);
+                robot.linearSlidesMotor2.setPower(0);
+            } else {
+                telemetry.addData("LS Direction", "DOWN");
+                robot.linearSlidesMotor1.setPower(-0.25 * gamepad2.right_trigger);
+                robot.linearSlidesMotor2.setPower(-0.25 * gamepad2.right_trigger);
+            }
+        } else if (gamepad2.left_trigger > 0.1) {
+            telemetry.addData("LS Direction", "UP");
+            robot.linearSlidesMotor1.setPower(0.5 * gamepad2.left_trigger);
+            robot.linearSlidesMotor2.setPower(0.5 * gamepad2.left_trigger);
+        } else {
+            telemetry.addData("LS Direction", "OFF");
+            robot.linearSlidesMotor1.setPower(0);
+            robot.linearSlidesMotor2.setPower(0);
+        }
+
+
         telemetry.addLine("Test Controls and Positions (GP1):");
         telemetry.addData("Claw Servo (X=inc, B=dec)", robot.clawServo.getPosition());
         telemetry.addData("Wrist Servo (Y=inc, A=dec)", robot.wristServo.getPosition());
         telemetry.addData("Elbow Servo (DPup=inc, DPdn=dec)", robot.elbowServo.getPosition());
         telemetry.addData("Garage Door Servo (LB=inc, RB=dec)", robot.elbowServo.getPosition());
+        telemetry.addData("Slides (LT=up, RT=dn)", robot.linearSlidesMotor1.getCurrentPosition());
         telemetry.update();
     }
 }
